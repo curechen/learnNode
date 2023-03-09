4b61fade040837d6be6b2cd401330476deafca79
curechen <981470148@qq.com>
2023-03-09 16:45:55 +0800

    feat: 对fs模块的读写模板进行修改，使用path.join方法来拼接路径

--- infrastructure/fs/read&write.js
+++ infrastructure/fs/read&write.js
@@ -1,11 +1,13 @@
 //    导入 fs 模块，来操作文件
+//    导入 path 模块，来拼接当前路径
 const fs = require('fs')
+const path = require('path')
 
 //    调用 fs.readFile() 方法读取文件
 //    参数1：必选参数，读取文件的存放路径
 //    参数2：可选参数，读取文件时候采用的编码格式，一般默认指定 utf8
 //    参数3：必选参数，回调函数，拿到读取失败和成功的结果  err  dataStr
-fs.readFile(__dirname + '/files/1.txt', 'utf8', function(err, dataStr) {
+fs.readFile(path.join(__dirname, '/files/1.txt'), 'utf8', function(err, dataStr) {
   // 2.1 打印失败的结果
   // 如果读取成功，则 err 的值为 null
   // 如果读取失败，则 err 的值为 错误对象，dataStr 的值为 undefined
@@ -20,7 +22,7 @@ fs.readFile(__dirname + '/files/1.txt', 'utf8', function(err, dataStr) {
 //    参数2：必选参数，表示要写入的内容
 //    参数3：可选参数，表示要写入的内容
 //    参数4：必选参数，回调函数
-fs.writeFile(__dirname + '/files/3.txt', 'ok123', function(err) {
+fs.writeFile(path.join(__dirname, '/files/3.txt'), 'ok123', function(err) {
   // 2.1 如果文件写入成功，则 err 的值等于 null
   // 2.2 如果文件写入失败，则 err 的值等于一个 错误对象
   // console.log(err)
