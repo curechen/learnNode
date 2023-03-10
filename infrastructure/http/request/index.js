2de1ad1991d626782dd7f23884b3f6c6b1549e97
curechen <981470148@qq.com>
2023-03-10 10:22:29 +0800

    feat: 添加http模块request基础模板

---
+++ infrastructure/http/request/index.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: curechen 981470148@qq.com
+ * @Date: 2023-03-10 10:21:11
+ * @LastEditors: curechen 981470148@qq.com
+ * @LastEditTime: 2023-03-10 10:21:21
+ * @FilePath: \workplace\node\infrastructure\http\request\index.js
+ * @Description: 
+ */
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+  // 1. 获取请求的 url 地址
+  const url = req.url
+  // 2. 设置默认的响应内容为 404 Not found
+  let content = '<h1>404 Not found!</h1>'
+  // 3. 判断用户请求的是否为 / 或 /index.html 首页
+  // 4. 判断用户请求的是否为 /about.html 关于页面
+  if (url === '/' || url === '/index.html') {
+    content = '<h1>首页</h1>'
+  } else if (url === '/about.html') {
+    content = '<h1>关于页面</h1>'
+  }
+  // 5. 设置 Content-Type 响应头，防止中文乱码
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  // 6. 使用 res.end() 把内容响应给客户端
+  res.end(content)
+})
+
+server.listen(80, () => {
+  console.log('server running at http://127.0.0.1')
+})
