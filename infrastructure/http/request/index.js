bdf86b94e0db67669df80d7476f57f9c9cd4f6e2
curechen <981470148@qq.com>
2023-03-13 14:47:28 +0800

    feat: 添加http模块request请求服务器资源的基础模板

--- infrastructure/http/request/index.js
+++ infrastructure/http/request/index.js
@@ -1,32 +1,43 @@
-/*
- * @Author: curechen 981470148@qq.com
- * @Date: 2023-03-10 10:21:11
- * @LastEditors: curechen 981470148@qq.com
- * @LastEditTime: 2023-03-10 10:21:21
- * @FilePath: \workplace\node\infrastructure\http\request\index.js
- * @Description: 
- */
+// 1.1 导入 http 模块
 const http = require('http')
+// 1.2 导入 fs 模块
+const fs = require('fs')
+// 1.3 导入 path 模块
+const path = require('path')
+
+// 2.1 创建 web 服务器
 const server = http.createServer()
-
+// 2.2 监听 web 服务器的 request 事件
 server.on('request', (req, res) => {
-  // 1. 获取请求的 url 地址
+  // 3.1 获取到客户端请求的 URL 地址
+  //     /clock/index.html
+  //     /clock/index.css
+  //     /clock/index.js
   const url = req.url
-  // 2. 设置默认的响应内容为 404 Not found
-  let content = '<h1>404 Not found!</h1>'
-  // 3. 判断用户请求的是否为 / 或 /index.html 首页
-  // 4. 判断用户请求的是否为 /about.html 关于页面
-  if (url === '/' || url === '/index.html') {
-    content = '<h1>首页</h1>'
-  } else if (url === '/about.html') {
-    content = '<h1>关于页面</h1>'
+  // 3.2 把请求的 URL 地址映射为具体文件的存放路径
+  // const fpath = path.join(__dirname, url)
+  // 5.1 预定义一个空白的文件存放路径
+  let fpath = ''
+  if (url === '/') {
+    fpath = path.join(__dirname, './clock/index.html')
+  } else {
+    //     /index.html
+    //     /index.css
+    //     /index.js
+    fpath = path.join(__dirname, '/clock', url)
   }
-  // 5. 设置 Content-Type 响应头，防止中文乱码
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  // 6. 使用 res.end() 把内容响应给客户端
-  res.end(content)
-})
 
+  // 4.1 根据“映射”过来的文件路径读取文件的内容
+  fs.readFile(fpath, 'utf8', (err, dataStr) => {
+    // 4.2 读取失败，向客户端响应固定的“错误消息”
+    if (err) return res.end('404 Not found.')
+    // res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    // 4.3 读取成功，将读取成功的内容，响应给客户端
+    res.end(dataStr)
+  })
+})
+// 2.3 启动服务器
 server.listen(80, () => {
   console.log('server running at http://127.0.0.1')
 })
